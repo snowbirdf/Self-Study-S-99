3bc385c719c3cbd865064cee1c31d341d7650761
snowbird <snowbird.and.townie+github@gmail.com>
2019-10-31 14:23:06 +0900

    add P04 answer and spec

---
+++ src/working_with_list/src/main/scala/P04.scala
@@ -0,0 +1,5 @@
+object P04 {
+  def length[T](list :List[T]): Int = {
+    list.length
+  }
+}
---
+++ src/working_with_list/src/test/scala/P04Spec.scala
@@ -0,0 +1,13 @@
+import org.scalatest.{DiagrammedAssertions, FlatSpec}
+
+class P04Spec extends FlatSpec with  DiagrammedAssertions {
+  "length" should "return list length" in {
+    assert(P04.length(List(1,2,3)) === 3)
+    assert(P04.length(List("a")) === 1)
+  }
+
+  it should "return 0 when empty List" in {
+    assert(P04.length(Nil) === 0)
+    assert(P04.length(List()) === 0)
+  }
+}
