38001cbf54131f06dfd5f1b10b3be9090fe3b5e9
snowbird <snowbird.and.townie+github@gmail.com>
2019-10-31 14:27:33 +0900

    add recursive method

--- src/working_with_list/src/main/scala/P04.scala
+++ src/working_with_list/src/main/scala/P04.scala
@@ -2,4 +2,12 @@ object P04 {
   def length[T](list :List[T]): Int = {
     list.length
   }
+
+  def lengthRecursive[T](list :List[T]): Int = {
+    list match {
+      case Nil => 0
+      case _ :: Nil => 1
+      case _ :: tail => 1 + lengthRecursive(tail)
+    }
+  }
 }
--- src/working_with_list/src/test/scala/P04Spec.scala
+++ src/working_with_list/src/test/scala/P04Spec.scala
@@ -10,4 +10,15 @@ class P04Spec extends FlatSpec with  DiagrammedAssertions {
     assert(P04.length(Nil) === 0)
     assert(P04.length(List()) === 0)
   }
+
+  "lengthRecursive" should "return list length" in {
+    assert(P04.lengthRecursive(List(1,2,3)) === 3)
+    assert(P04.lengthRecursive(List("a")) === 1)
+  }
+
+  it should "return 0 when empty List" in {
+    assert(P04.lengthRecursive(Nil) === 0)
+    assert(P04.lengthRecursive(List()) === 0)
+  }
+
 }
