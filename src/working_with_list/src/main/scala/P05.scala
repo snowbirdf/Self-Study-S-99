fceb724455ef61c9e65925ed2fbcb3ad40a1d965
snowbird <snowbird.and.townie+github@gmail.com>
2019-10-31 14:52:02 +0900

    add recursive style

--- src/working_with_list/src/main/scala/P05.scala
+++ src/working_with_list/src/main/scala/P05.scala
@@ -3,5 +3,12 @@ object P05 {
     list.reverse
   }
 
+  def reverseRecursive[T](list: List[T]) :List[T] = {
+    list match {
+      case Nil => Nil
+      case x :: tail => reverseRecursive(tail) ++ List(x)
+    }
+  }
+
 
 }
--- src/working_with_list/src/test/scala/P05Spec.scala
+++ src/working_with_list/src/test/scala/P05Spec.scala
@@ -8,4 +8,9 @@ class P05Spec extends FlatSpec with DiagrammedAssertions {
     assert(P05.reverse(List(1,2,3,4,5)) === List(5,4,3,2,1))
     assert(P05.reverse(Nil) === Nil)
   }
+
+  it should "reverseRecursive argument list" in  {
+    assert(P05.reverseRecursive(List(1,2,3,4,5)) === List(5,4,3,2,1))
+    assert(P05.reverseRecursive(Nil) === Nil)
+  }
 }
