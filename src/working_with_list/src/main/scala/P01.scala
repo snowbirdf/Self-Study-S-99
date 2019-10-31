496a6eca941e0a393b6f0919aae8c76342bfb447
snowbird <snowbird.and.townie+github@gmail.com>
2019-10-31 12:55:11 +0900

    add recursive type answer

--- src/working_with_list/src/main/scala/P01.scala
+++ src/working_with_list/src/main/scala/P01.scala
@@ -1,5 +1,15 @@
-object P01 {
+object P01 extends App {
+
   def last[A](lst: List[A]) : A  = {
     lst.last
   }
+
+  def lastRecursive[A](list : List[A]): A = {
+    list match {
+      case head :: Nil => head
+      case _ :: tail => lastRecursive(tail)
+      case _ => throw new NoSuchElementException()
+    }
+  }
+
 }
--- src/working_with_list/src/test/scala/P01Spec.scala
+++ src/working_with_list/src/test/scala/P01Spec.scala
@@ -17,4 +17,20 @@ class P01Spec extends FlatSpec with DiagrammedAssertions {
     }
   }
 
+  "lastRecursive"  should "return last element of list" in {
+    assert(P01.last(List(1,1,2,3,4,8)) === 8)
+    assert(P01.last(List("A","b","C", "D", "E", "f")) === "f")
+    assert(P01.last(List(1.1,2.00,0.18,3.14)) === 3.14)
+  }
+
+  it should "throw Exception when argument List is Nil or No Element" in {
+    intercept[NoSuchElementException]{
+      P01.last(Nil)
+    }
+
+    intercept[NoSuchElementException]{
+      P01.last(List())
+    }
+  }
+
 }
