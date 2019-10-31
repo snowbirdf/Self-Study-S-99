b8d6be6f602de4206ba4f24c82a678be11024ab2
snowbird <snowbird.and.townie+github@gmail.com>
2019-10-31 14:33:33 +0900

    add recursive only tail

--- src/working_with_list/src/main/scala/P04.scala
+++ src/working_with_list/src/main/scala/P04.scala
@@ -10,4 +10,14 @@ object P04 {
       case _ :: tail => 1 + lengthRecursive(tail)
     }
   }
+
+  def lengthTailRecursive[T](list :List[T]):Int = {
+    def tailRecursive(result: Int, list:List[T]): Int = list match {
+      case Nil => result
+      case _ :: Nil => result + 1
+      case _ :: tail => tailRecursive(result + 1, tail)
+    }
+
+    tailRecursive(0, list)
+  }
 }
--- src/working_with_list/src/test/scala/P04Spec.scala
+++ src/working_with_list/src/test/scala/P04Spec.scala
@@ -21,4 +21,14 @@ class P04Spec extends FlatSpec with  DiagrammedAssertions {
     assert(P04.lengthRecursive(List()) === 0)
   }
 
+  "lengthTailRecursive" should "return list length" in {
+    assert(P04.lengthTailRecursive(List(1,2,3)) === 3)
+    assert(P04.lengthTailRecursive(List("a")) === 1)
+  }
+
+  it should "return 0 when empty List" in {
+    assert(P04.lengthTailRecursive(Nil) === 0)
+    assert(P04.lengthTailRecursive(List()) === 0)
+  }
+
 }
