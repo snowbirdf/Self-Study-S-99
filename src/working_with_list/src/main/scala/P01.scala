2193333e384382426c7aa75369bd8925267f2172
snowbird <snowbird.and.townie+github@gmail.com>
2019-10-31 12:40:02 +0900

    answer and spec of P01

---
+++ src/working_with_list/src/main/scala/P01.scala
@@ -0,0 +1,5 @@
+object P01 {
+  def last[A](lst: List[A]) : A  = {
+    lst.last
+  }
+}
---
+++ src/working_with_list/src/test/scala/P01Spec.scala
@@ -0,0 +1,20 @@
+import org.scalatest.{DiagrammedAssertions, FlatSpec}
+
+class P01Spec extends FlatSpec with DiagrammedAssertions {
+  "last"  should "return last element of list" in {
+    assert(P01.last(List(1,1,2,3,4,8)) === 8)
+    assert(P01.last(List("A","b","C", "D", "E", "f")) === "f")
+    assert(P01.last(List(1.1,2.00,0.18,3.14)) === 3.14)
+  }
+
+  it should "throw Exception when argument List is Nil or No Element" in {
+    intercept[NoSuchElementException]{
+      P01.last(Nil)
+    }
+
+    intercept[NoSuchElementException]{
+      P01.last(List())
+    }
+  }
+
+}
